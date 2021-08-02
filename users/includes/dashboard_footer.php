8b34d0c121de05c8b2e581cda6d9499507b33e54
Mohd Wasim <77122091+spideyhere@users.noreply.github.com>
2021-08-03 00:29:12 +0530

    Add files via upload

---
+++ users/includes/dashboard_footer.php
@@ -0,0 +1,16 @@
+ 
+    <!-- Core Vendors JS -->
+    <script src="assets/js/vendors.min.js"></script>
+
+    <!-- page js -->
+    <script src="assets/vendors/chartjs/Chart.min.js"></script>
+    <script src="assets/js/pages/dashboard-default.js"></script>
+
+    <!-- Core JS -->
+    <script src="assets/js/app.min.js"></script>
+
+</body>
+
+
+<!-- Mirrored from www.themenate.net/enlink-bs/dist/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 23 Aug 2020 12:06:52 GMT -->
+</html>
---
+++ users/includes/dashboard_sidenav.php
@@ -0,0 +1,101 @@
+ <!-- Side Nav START -->
+            <div class="side-nav">
+                <div class="side-nav-inner">
+                    <ul class="side-nav-menu scrollable">
+                        <li class="nav-item dropdown open">
+                            <a class="dropdown-toggle" href="index.html">
+                                <span class="icon-holder">
+                                    <i class="anticon anticon-dashboard"></i>
+                                </span>
+                                <span class="title">Dashboard</span>
+                            </a>                            
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" href="javascript:void(0);">
+                                <span class="icon-holder">
+                                    <i class="anticon anticon-appstore"></i>
+                                </span>
+                                <span class="title">Products</span>
+                                <span class="arrow">
+                                    <i class="arrow-icon"></i>
+                                </span>
+                            </a>
+                            <ul class="dropdown-menu">
+                               
+                                <li class="nav-item dropdown">
+                                    <a href="javascript:void(0);">
+                                        <span>Clothes</span>
+                                        <span class="arrow">
+                                            <i class="arrow-icon"></i>
+                                        </span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="app-project-list.html">Mens Clothes</a>
+                                        </li>
+                                        <li>
+                                            <a href="women_clothes.php">Womens Clothes</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a href="javascript:void(0);">
+                                        <span>Shoes</span>
+                                        <span class="arrow">
+                                            <i class="arrow-icon"></i>
+                                        </span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="app-e-commerce-order-list.html">Mens Shoes</a>
+                                        </li>
+                                        <li>
+                                            <a href="app-e-commerce-products.html">Womens Shoes</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                       
+                        
+                        <li class="nav-item dropdown">
+                            <a class="dropdown-toggle" href="javascript:void(0);">
+                                <span class="icon-holder">
+                                    <i class="anticon anticon-lock"></i>
+                                </span>
+                                <span class="title">Authentication</span>
+                                <span class="arrow">
+                                    <i class="arrow-icon"></i>
+                                </span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="login-1.html">Login 1</a>
+                                </li>
+                                <li>
+                                    <a href="login-2.html">Login 2</a>
+                                </li>
+                                <li>
+                                    <a href="login-3.html">Login 3</a>
+                                </li>
+                                <li>
+                                    <a href="sign-up-1.html">Sign Up 1</a>
+                                </li>
+                                <li>
+                                    <a href="sign-up-2.html">Sign Up 2</a>
+                                </li>
+                                <li>
+                                    <a href="sign-up-3.html">Sign Up 3</a>
+                                </li>
+                                <li>
+                                    <a href="error-1.html">Error 1</a>
+                                </li>
+                                <li>
+                                    <a href="error-2.html">Error 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Side Nav END -->
